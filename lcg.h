05417f80e8c07f59f92ef2a51c189e0a358c6111
Ashit Rustagi <me.ashitrustagi@gmail.com>
2020-04-11 21:47:41 +0530

    second implemented

--- lcg.h
+++ lcg.h
@@ -11,6 +11,12 @@ protected:
 	int b = 2147483587;
 public:
 	vector<long long> generate_random_numbers(int count, int seed);
+	long long mod_mult(long long a,long long b,long long mod)
+	{
+		return ((a%mod)*(b%mod))%mod;
+	}
+	long long mult(long long x,long long y,long long m) { long long prod = 1; while(y>0){ if(y&1) prod = mod_mult(prod,x,m); x=mod_mult(x,x,m); y/=2;} return prod;}
+	long long inv_mod(long long x,long long y,long long m){ return mod_mult(x,mult(y,m-2,m),m);}
 };
 
 #endif
