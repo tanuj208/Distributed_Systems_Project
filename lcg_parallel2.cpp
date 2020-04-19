edc49c729cf88a86f2ff403e582e0bf834437c00
Tanuj Garg <tanujgarg@pop-os.localdomain>
2020-04-19 20:57:49 +0530

    Idea 3 implemented

--- lcg_parallel2.cpp
+++ lcg_parallel2.cpp
@@ -8,19 +8,19 @@ LCG_Parallel2 :: LCG_Parallel2(int number_of_processes, int rank)
 
 vector<long long> LCG_Parallel2 :: generate_random_numbers(int count, int seed)
 {
-	vector<long long> tmp;
+	vector<long long> random_nums;
 	long long start;
 	int n = count/num_procs;
-	long long x = mod_mult(a,prank*num_procs,mod);
+	long long x = power(a,prank*num_procs,mod);
 	if(prank==0)
 		start = seed;
 	else
 		start = ( mod_mult(x,seed,mod) + mod_mult(inv_mod(x-1,a-1,mod),b,mod))%mod;
-	tmp.push_back(start);
+	random_nums.push_back(start);
 	for(int i =1; i<n; i++)
 	{
 		start = ( mod_mult(a,start,mod) + b%mod)%mod;
-		tmp.push_back(start);
+		random_nums.push_back(start);
 	}
-	return tmp;
+	return random_nums;
 }
--- lcg_parallel3.cpp
+++ lcg_parallel3.cpp
@@ -6,9 +6,43 @@ LCG_Parallel3 :: LCG_Parallel3(int number_of_processes, int rank)
 	prank = rank;
 }
 
+long long LCG_Parallel3 :: get_pth_random_no(int p, int seed)
+{
+	long long x = power(a, p, mod);
+	long long r_no = (mod_mult(x, p, mod) + mod_mult(b, inv_mod(x-1, a-1, mod), mod))%mod;
+	return r_no;
+}
+
+int LCG_Parallel3 :: distribute_task(int total_work)
+{
+	int base_task = total_work / num_procs;
+	int more_work_proc = total_work - base_task * num_procs;
+	int nums_to_gen = base_task;
+
+	if(prank < more_work_proc)
+		nums_to_gen ++;
+
+	return nums_to_gen;
+}
+
+vector<long long> LCG_Parallel3 :: gen_nums(int seed, int cnt)
+{
+	vector<long long> random_nums;
+	int i;
+	long long last_num_generated = seed;
+	for(i=0;i<cnt;i++)
+	{
+		long long new_num = (mod_mult(last_num_generated, a2, mod) + b2) % mod;
+		random_nums.push_back(new_num);
+		last_num_generated = new_num;
+	}
+	return random_nums;
+}
+
 vector<long long> LCG_Parallel3 :: generate_random_numbers(int count, int seed)
 {
-	cout<<"Method 3"<<endl;
-	vector<long long> tmp;
-	return tmp;
+	long long new_seed = get_pth_random_no(prank+1, seed);
+	int nums_to_gen = distribute_task(count);
+	vector<long long> random_nums = gen_nums(new_seed, nums_to_gen);
+	return random_nums;
 }
